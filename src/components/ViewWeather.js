98273c2b7688ee363fa7ef4ec4b0fe74032b058b
Adam Toczko <adamtoczko80@gmail.com>
2019-11-27 18:48:31 +0100

    created SearchCity and ViewWeather components, installed axios

---
+++ src/components/ViewWeather.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import axios from axios;
+
+// Nazwie miasta, dla którego jest wyświetlana pogoda
+// Położeniu geolokalizacyjnym miasta (latitude, longitude)
+// Aktualnej temperaturze w danej chwili
+// Zakresie temperatur z danego dnia
+// Aktualnej wilgotności oraz ciśnieniu atmosferycznym
+// Aktualnych opadach oraz wietrze
---
+++ src/components/notes.js
@@ -0,0 +1,12 @@
+// http://api.openweathermap.org/data/2.5/weather?q=Gdansk&units=metric&appid=05508bb378ad891b493b0c886cca7a57
+// Stworzyć w apce reacta komponent SearchCity, który zawiera:
+// input tekstowy do wpisania nazwy miasta
+// Button do obsługi zapytania axiosa z wpisaną przez użytkownika nazwą miasta
+// Stworzyć w apce react komponent ViewWeather, który zawiera informacje o:
+// Nazwie miasta, dla którego jest wyświetlana pogoda
+// Położeniu geolokalizacyjnym miasta (latitude, longitude)
+// Aktualnej temperaturze w danej chwili
+// Zakresie temperatur z danego dnia
+// Aktualnej wilgotności oraz ciśnieniu atmosferycznym
+// Aktualnych opadach oraz wietrze
+
