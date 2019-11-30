e55dceea50551c8b6cebf74656642509b551bc04
Adam Toczko <adamtoczko80@gmail.com>
2019-11-30 13:06:49 +0100

    had to change componenst from class to function and move fetching to App, got it working , needs styling, errors etc

--- src/App.js
+++ src/App.js
@@ -1,20 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
 import ViewWeather from './components/ViewWeather';
 import SearchCity from './components/SearchCity';
 
-function App() {
+class App extends React.Component {
+  
+  state = {
+    value:'',
+    long: '',
+    lati: '',
+    weatherDescription: '',
+    weatherIcon: '',
+    temp: '',
+    pressure: '',
+    humidity: '',
+    tempMin: '',
+    tempMax: '',
+    windSpeed: '',
+    clouds: '',
+    sunrise: '',
+    sunset: '',
+    
+    }
+
+    onInputChange =(event) => {
+      const {value} = event.target
+  
+      this.setState({value: value})
+  
+      console.log({value});
+  }
+
+
+    handleSubmit = e => {
+		e.preventDefault();
+
+      axios
+        .get(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&units=metric&appid=05508bb378ad891b493b0c886cca7a57`)
+        .then(response => {
+            console.log(response.data)
+          return response.data})
+          .then(data =>
+            this.setState(state => ({
+      
+                long: data.coord.lon,
+                lati: data.coord.lat,
+                weatherDescription: data.weather[0].description,
+                weatherIcon:data.weather[0].icon,
+                temp: data.main.temp,
+                pressure: data.main.pressure,
+                humidity: data.main.humidity,
+                tempMin: data.main.temp_min,
+                tempMax: data.main.temp_max,
+                windSpeed: data.wind.speed,
+                clouds: data.weather[0].main,
+                sunrise: data.sys.sunrise,
+                sunset: data.sys.sunset,
+            }))
+            );
+        }
+
+  
+
+
+  render() {
   return (
     <div className="App">
-      {/* <div className="AppShadow"> */}
-      <ViewWeather />
-      <SearchCity />
+      <div className="AppShadow">
+      <ViewWeather 
+      weather={this.state}
+      />
       
-      {/* </div> */}
+      <SearchCity 
+      value={this.state.value}
+      change={this.onInputChange}
+      submit={this.handleSubmit}
+      />
+      
+      </div>
     </div>
   );
+}
 }
 
 export default App;
--- src/components/SearchCity.js
+++ src/components/SearchCity.js
@@ -3,34 +3,34 @@ import React from 'react';
 // input tekstowy do wpisania nazwy miasta
 // Button do obsługi zapytania axiosa z wpisaną przez użytkownika nazwą miasta
 
-class SearchCity extends React.Component{
-    constructor(props){
-        super(props)
+function SearchCity (props) {
+   
+    // onInputChange =(event) => {
+    //     const {value} = event.target
     
-        this.state = {inputValue: ''}
-    }
+    //     this.setState({inputValue: value})
     
-    onInputChange =(event) => {
-        const {value} = event.target
-    
-        this.setState({inputValue: value})
-    
-        console.log({value});
-    }
-    
-    render(){
-    
-    const {inputValue} = this.state;
+    //     console.log({value});
+    // }
     
     return (
         <div>
-        <input type='text' placeholder='Wpisz tekst' value={inputValue} onChange={this.onInputChange} />
-        <span> Pokaz co wpisuje:  <span style={{color: 'yellow'}}>{inputValue}</span> </span>
-        <button type="button" className="btn btn-primary">Primary</button>
+        <form onSubmit={props.submit}>
+        <input type='text' placeholder='Wpisz miasto' value={props.value} onChange={props.change} />
+        <span> Pokaz co wpisuje:  <span style={{color: 'yellow'}}>{props.value}</span> </span>
+        
+        {/* <div className="form-group row">
+        <div className="col-xs-2">
+        <label htmlFor="ex1">col-xs-2</label>
+        <input className="form-control" id="ex1" type="text" />
         </div>
+        </div> */}
+        <button type="submit" className="btn btn-primary">Pobierz pogodę</button>
+        </form>
+      </div>
     )
     
     }
-    }
+
 
     export default SearchCity;
