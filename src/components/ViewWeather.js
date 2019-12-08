74055593648a85ba72b7cdc9355e4b213ed8476e
Adam Toczko <adamtoczko80@gmail.com>
2019-12-08 16:02:36 +0100

    without timezone for now

--- src/components/ViewWeather.js
+++ src/components/ViewWeather.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import moment from 'moment'
 
 function ViewWeather (props) {
       const {long, lati, weatherDescription, weatherIcon, temp, pressure, humidity,
@@ -8,7 +8,7 @@ function ViewWeather (props) {
     
     let Sunrise = new Date(sunrise*1000).toLocaleTimeString();
     let Sunset = new Date(sunset*1000).toLocaleTimeString();
-
+        
 
     if (dataNotLoaded && !error) {
       return (
@@ -21,7 +21,7 @@ function ViewWeather (props) {
           
               
             <div>
-            <div className="col-12 col-sm-12">Current Time: <span style={{color: 'yellow'}}>{timezone}</span> </div>
+            {/* <div className="col-12 col-sm-12">Current Time: {n} <span style={{color: 'yellow'}}>{timezone}</span> </div> */}
             <div className="w-100 d-none d-md-block"></div>
             <div className="row">
             <div className="col-6 col-sm-4">City: <span style={{color: 'yellow'}}>{city}</span> </div>
