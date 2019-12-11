b751515bdeb89d9f4d994f7211bd39912c6238bd
Adam Toczko <adamtoczko80@gmail.com>
2019-12-11 07:58:46 +0100

    Updates

---
+++ precache-manifest.1236dec74b17d1e3069b6bc915f67809.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4e099ca6b53d4c43b264f275384eda8",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "56f8e6f96c5d866a675a",
+    "url": "/Weather-App/static/css/main.d12f1755.chunk.css"
+  },
+  {
+    "revision": "bc1400925140a33d6837",
+    "url": "/Weather-App/static/js/2.41f22763.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/Weather-App/static/js/2.41f22763.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56f8e6f96c5d866a675a",
+    "url": "/Weather-App/static/js/main.0d1320dc.chunk.js"
+  },
+  {
+    "revision": "f7a0b8093de90b86c245",
+    "url": "/Weather-App/static/js/runtime-main.2f2b1e57.js"
+  }
+]);
